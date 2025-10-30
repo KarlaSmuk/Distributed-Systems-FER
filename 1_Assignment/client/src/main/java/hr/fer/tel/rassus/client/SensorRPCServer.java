4681e055f656223c3d8ae83c4c68fc4e34558e85
KarlaSmuk <karla.smuk@gmail.com>
2025-10-30 23:32:16 +0100

    update sensor service and proto

--- 1_Assignment/client/src/main/java/hr/fer/tel/rassus/client/SensorRPCServer.java
+++ 1_Assignment/client/src/main/java/hr/fer/tel/rassus/client/SensorRPCServer.java
@@ -1,81 +1,79 @@
 package hr.fer.tel.rassus.client;
 
-import io.grpc.BindableService;
 import io.grpc.Server;
 import io.grpc.ServerBuilder;
+
 import java.io.IOException;
 import java.util.concurrent.TimeUnit;
 import java.util.logging.Logger;
 
 public class SensorRPCServer {
-  private static final Logger logger = Logger.getLogger(SensorRPCServer.class.getName());
+    private static final Logger logger = Logger.getLogger(SensorRPCServer.class.getName());
 
-  private Server server;
-  private final SensorService sensorService;
-  private final int port;
+    private Server server;
+    private final int port;
 
-  public SensorRPCServer(SensorService service, int port) {
-    this.sensorService = service;
-    this.port = port;
-  }
-
-  /**
-   * Start the server.
-   *
-   * @throws IOException the io exception
-   */
-  public void start() throws IOException {
-    // Register the service
-    server = ServerBuilder.forPort(port)
-        .addService((BindableService) sensorService)
-        .build()
-        .start();
-    logger.info("Server started on " + port);
-
-    //  Clean shutdown of server in case of JVM shutdown
-    Runtime.getRuntime().addShutdownHook(new Thread(() -> {
-      System.err.println("Shutting down gRPC server since JVM is shutting down");
-      try {
-        SensorRPCServer.this.stop();
-      } catch (InterruptedException e) {
-        e.printStackTrace(System.err);
-      }
-      System.err.println("Server shut down");
-    }));
-  }
-
-  /**
-   * Stops the server.
-   *
-   * @throws InterruptedException the interrupted exception
-   */
-  public void stop() throws InterruptedException {
-    if (server != null) {
-      server.shutdown().awaitTermination(30, TimeUnit.SECONDS);
+    public SensorRPCServer(int port) {
+        this.port = port;
     }
-  }
 
-  /**
-   * Await termination on the main thread
-   *
-   * @throws InterruptedException the interrupted exception
-   */
-  public void blockUntilShutdown() throws InterruptedException {
-    if (server != null) {
-      server.awaitTermination();
+    /**
+     * Start the server.
+     *
+     * @throws IOException the io exception
+     */
+    public void start() throws IOException {
+        // Register the service
+        server = ServerBuilder.forPort(port)
+                .addService(new SensorService())
+                .build()
+                .start();
+        logger.info("Server started on " + port);
+
+        //  Clean shutdown of server in case of JVM shutdown
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            System.err.println("Shutting down gRPC server since JVM is shutting down");
+            try {
+                SensorRPCServer.this.stop();
+            } catch (InterruptedException e) {
+                e.printStackTrace(System.err);
+            }
+            System.err.println("Server shut down");
+        }));
     }
-  }
 
-  /**
-   * The entry point of application.
-   *
-   * @param args the input arguments
-   * @throws IOException          the io exception
-   * @throws InterruptedException the interrupted exception
-   */
-  public static void main(String[] args) throws IOException, InterruptedException {
-    final SensorRPCServer server = new SensorRPCServer(new SensorRPCClient("127.0.0.1", 3000), 3000);
-    server.start();
-    server.blockUntilShutdown();
-  }
+    /**
+     * Stops the server.
+     *
+     * @throws InterruptedException the interrupted exception
+     */
+    public void stop() throws InterruptedException {
+        if (server != null) {
+            server.shutdown().awaitTermination(30, TimeUnit.SECONDS);
+        }
+    }
+
+    /**
+     * Await termination on the main thread
+     *
+     * @throws InterruptedException the interrupted exception
+     */
+    public void blockUntilShutdown() throws InterruptedException {
+        if (server != null) {
+            server.awaitTermination();
+        }
+    }
+
+    /**
+     * The entry point of application.
+     *
+     * @param args the input arguments
+     * @throws IOException          the io exception
+     * @throws InterruptedException the interrupted exception
+     */
+    public static void main(String[] args) throws IOException, InterruptedException {
+        final SensorRPCServer server = new SensorRPCServer(3000);
+        server.start();
+        server.blockUntilShutdown();
+    }
 }
--- 1_Assignment/client/src/main/java/hr/fer/tel/rassus/client/SensorService.java
+++ 1_Assignment/client/src/main/java/hr/fer/tel/rassus/client/SensorService.java
@@ -1,5 +1,6 @@
 package hr.fer.tel.rassus.client;
 
+import hr.fer.tel.rassus.client.model.Reading;
 import io.grpc.stub.StreamObserver;
 
 public class SensorService extends SensorGrpc.SensorImplBase {
@@ -7,13 +8,13 @@ public class SensorService extends SensorGrpc.SensorImplBase {
     @Override
     public void requestSensor(SensorDataRequest request, StreamObserver<SensorDataResponse> responseObserver) {
 
-        SensorDataResponse response = SensorDataResponse.newBuilder().build();
-//                .setTemperature()
-//                .setPressure()
-//                .setHumidity()
-//                .setCO()
-//                .setNO2()
-//                .build();
+        SensorDataResponse response = SensorDataResponse.newBuilder()
+                .setTemperature(reading.getTemperature())
+                .setPressure(reading.getPressure())
+                .setHumidity(reading.getHumidity())
+                .setCO(reading.getCo())
+                .setNO2(reading.getNo2())
+                .build();
 
         responseObserver.onNext(response);
         responseObserver.onCompleted();
